59b812cf1ed1e23da8b2b777873b658375929b43
killbozer <35951632+killbozer@users.noreply.github.com>
2019-10-15 23:27:21 +0930

    Scaffold Identity

---
+++ Assignment2/Assignment2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Assignment2.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Assignment2.Areas.Identity.IdentityHostingStartup))]
+namespace Assignment2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
