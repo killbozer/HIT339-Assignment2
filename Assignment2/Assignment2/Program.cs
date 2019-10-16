24a0723e3b057194f74cd633e591001f0d30b724
killbozer <35951632+killbozer@users.noreply.github.com>
2019-10-16 18:05:38 +0930

    Fix no dependency in program.cs

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Assignment2.Data;
 
 namespace Assignment2
 {
